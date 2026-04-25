1a1ec0a3fb575894466e0e66d9e813c32430964e
Mario Lang <mario@mlwx.de>
2026-04-25 11:48:36 +0000

    feat: add default view

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import OverlayView from './views/OverlayView.vue'
 import DemoView from './views/DemoView.vue'
+import HomeView from './views/HomeView.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: '/',
+      component: HomeView,
+    },
     {
       path: '/demo',
       component: DemoView,
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="grid size-full items-end pb-8 text-center text-5xl font-extrabold text-red-500 drop-shadow-md/100"
+  >
+    No channel name provided
+  </div>
+</template>
